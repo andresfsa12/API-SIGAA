739af5c7f54aac21347beb10ae21b2c34ab99d6f
andresfsa12 <andresfsa12@gmail.com>
2024-09-01 10:30:12 -0500

    Interacción con base de datos

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+app.use(cors())
+
 // Get the client
 const mysql = require('mysql2/promise');
 
 // Create the connection to database
 const connection = mysql.createPool({
   host: 'localhost',
+  
   user: 'root',
-  database: 'login',
+  database: 'mydb',
 });
 
+/////////////////////////////
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/login', (req, res) => { //req = request = petición; res = response = respuesta;
+app.get('/mydb',async (req, res) => { //req = request = petición; res = response = respuesta;
   const datos = req.query;
-  console.log(datos);
-  res.send('Inicio Sesion')
+
+  try {
+    const [results, fields] = await connection.query(
+      "SELECT * FROM `Acudiente` WHERE `N_id` = ? AND `PASSWORD` = ?",
+      [datos.N_id,datos.PASSWORD]
+    );
+
+  if (results.length > 0 ){
+    res.status(200).send('Inicio de sesión correcto')
+      }else{
+        res.status(401).send('Datos incorrectos')
+  };
+
+  } catch (err) {
+    console.log(err);
+  }
+
 })
 
 app.get('/validate', (req, res) => {
