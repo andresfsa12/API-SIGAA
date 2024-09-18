0dd75cdd69bc4eb7f43f70c14e46afbaf1f393a5
andresfsa12 <andresfsa12@gmail.com>
2024-09-17 22:01:59 -0500

    Validación de los 3 usuarios

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const session = require('express-session');
 const mysql = require('mysql2/promise');
 
+
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
@@ -28,19 +29,45 @@ const connection = mysql.createPool({
 
 /////////////////////////////
 
-async function login(req, res) { //req = request = petición; res = response = respuesta;
-  const datos = req.query;
-  const [filas] = await connection.query("SELECT * FROM `acudiente` WHERE `N_id` = '"+ datos.N_id + "'AND `PASSWORD` = '"+ datos.PASSWORD+"'");
+async function loginAcudiente(req, res) { //req = request = petición; res = response = respuesta;
+  const datosAcudiente = req.query;
+  const [filas] = await connection.query("SELECT * FROM `acudiente` WHERE `N_id` = '"+ datosAcudiente.N_id + "'AND `PASSWORD` = '"+ datosAcudiente.PASSWORD+"'");
       
   if (filas.length == 1 ){
-    req.session.usuarios = datos.N_id;
+    req.session.usuarios = datosAcudiente.N_id;
     res.status(200).json({logueado:true})
       }else{
         res.status(401).json({error: 'Datos incorrectos'})
-  };
+      }}
+async function loginDocente(req, res) { //req = request = petición; res = response = respuesta;
+  const datosDocente = req.query;
+  const [filas] = await connection.query("SELECT * FROM `docente` WHERE `Id_Docente` = '"+ datosDocente.Id_Docente + "'AND `Clave` = '"+ datosDocente.Clave+"'");
+      
+      if (filas.length == 1 ){
+        req.session.usuarios = datosDocente.Id_Docente;
+        res.status(200).json({logueado:true})
+          }else{
+            res.status(401).json({error: 'Datos incorrectos'})
+          }
+    }
+
+    async function loginEstudiante(req, res) { //req = request = petición; res = response = respuesta;
+    const datosEstudiante = req.query;
+    const [filas] = await connection.query("SELECT * FROM `estudiante` WHERE `Id_Estudiante` = '"+ datosEstudiante.Id_Estudiante + "'AND `Clave` = '"+ datosEstudiante.Clave+"'");
+        
+        if (filas.length == 1 ){
+          req.session.usuarios = datosEstudiante.Id_Estudiante;
+          res.status(200).json({logueado:true})
+            }else{
+              res.status(401).json({error: 'Datos incorrectos'})
+            }
+    return usuarios;
+  
 return;}
 
-app.get('/login',login)
+app.get('/login-Acudiente',loginAcudiente)
+app.get('/login-Docente',loginDocente)
+app.get('/login-Estudiante',loginEstudiante)
 
 function validar(req,res){
   if (req.session.usuarios){
